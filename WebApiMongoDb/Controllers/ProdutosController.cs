ad4c9299aaab915136ad43cf72a3b041f3204864
IlkaCarmo <ilka.scarmo@gmail.com>
2025-02-19 15:04:55 -0300

    Add project files.

---
+++ WebApiMongoDb/Controllers/ProdutosController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiMongoDb.Models;
+using WebApiMongoDb.Services;
+
+namespace WebApiMongoDb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly Produtoservices _produtoservices;
+
+        public ProdutosController(Produtoservices produtoservices)
+        {
+            _produtoservices = produtoservices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Produto>>> GetProdutos()
+        {
+            var produtos = await _produtoservices.GetProdutosAsync();
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(string id)
+        {
+            var produto = await _produtoservices.GetProdutoAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> CreateProduto(Produto produto)
+        {
+            await _produtoservices.CreateAsync(produto);
+            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduto(string id, Produto produto)
+        {
+            var produtoAtual = await _produtoservices.GetProdutoAsync(id);
+            if (produtoAtual == null)
+            {
+                return NotFound();
+            }
+            await _produtoservices.UpdateAsync(id, produto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduto(string id)
+        {
+            var produto = await _produtoservices.GetProdutoAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            await _produtoservices.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ WebApiMongoDb/Services/ProdutoServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WebApiMongoDb.Models;
+
+namespace WebApiMongoDb.Services
+{
+    public class Produtoservices
+    {
+        private readonly IMongoCollection<Produto> _produtoCollection;
+
+        public Produtoservices(IOptions<ProdutoDatabaseSettings> produtoservices)
+        {
+            var mongoClient = new MongoClient(produtoservices.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(produtoservices.Value.DatabaseName);
+
+            _produtoCollection = mongoDatabase.GetCollection<Produto>(produtoservices.Value.ProdutoCollectionName);
+        }
+
+        public async Task<List<Produto>> GetProdutosAsync() => 
+            await _produtoCollection.Find(produto => true).ToListAsync();
+
+        public async Task<Produto> GetProdutoAsync(string id) =>
+            await _produtoCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Produto produto) =>
+            await _produtoCollection.InsertOneAsync(produto);
+
+        public async Task UpdateAsync(string id, Produto produto) =>
+            await _produtoCollection.ReplaceOneAsync(x => x.Id == id, produto);
+
+        public async Task DeleteAsync(string id) =>
+            await _produtoCollection.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
